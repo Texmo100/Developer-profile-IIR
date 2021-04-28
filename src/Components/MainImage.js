f4370fcc72425988e0b3e2b04d94420d7088d9f0
Texmo100 <isaac.ir@hotmail.com>
2021-04-27 17:04:20 -0700

    profile-idea-2 no sticky-bar-menu

--- src/Components/MainImage.js
+++ src/Components/MainImage.js
@@ -1,16 +1,10 @@
 import React, {Component} from 'react'
-import StickyNav from './StickyNav'
-
 
 class MainImage extends Component{
     render(){
         return(
             <div className="main-image">
-                <div className="image-layer">
-                    <div className="container">
-                        <StickyNav />
-                    </div>
-                </div>
+                <div className="image-layer"></div>
             </div>
         )
     }
