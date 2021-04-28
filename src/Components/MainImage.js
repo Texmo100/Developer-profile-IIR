c543dde368a093249b9d4c4f9e0b765d36a65cc0
Texmo100 <isaac.ir@hotmail.com>
2021-04-28 15:13:27 -0700

    Navbar second attempt

--- src/Components/MainImage.js
+++ src/Components/MainImage.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
+import NavBar from './Navbar'
 
 class MainImage extends Component{
     render(){
         return(
             <div className="main-image">
-                <div className="image-layer"></div>
+                <div className="image-layer">
+                    <div className="container">
+                        <NavBar />
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
