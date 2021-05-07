b085aaf098ae4e63556e4492e9aebe6c37829f5b
Texmo100 <isaac.ir@hotmail.com>
2021-05-06 17:02:19 -0700

    Projects section Done, navbar redesign and mobile version fixed

--- src/Components/CardProject.js
+++ src/Components/CardProject.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { FaCode } from 'react-icons/fa';
 
 const CardProject = () => {
     return(
         <div className="card-project">
-            <div className="image-project"></div>
+            <div className="image-project">
+                <FaCode style={{fontSize: "10rem", color: "#aeaeae"}}/>
+            </div>
             <div className="project-info">
-                <p className="project-title">project title</p>
+                <p className="project-title">Project title</p>
                 <p className="project-desc">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
