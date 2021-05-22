036ffea884889f8ead7884b8be1868de98fe01dc
Texmo100 <isaac.ir@hotmail.com>
2021-05-22 12:43:38 -0700

    Data added to the project correctly

---
+++ src/Data/aboutMeData.js
@@ -0,0 +1,53 @@
+const aboutMeData = {
+    profile: "I'm Isaac I.R. a software engineer junior apasionated, creatived, meticulous and in constantly improving coding skills everyday. Currently searching for a job oportunity as a software developer to improve and get real-life experience throught new coding challenges and solving any problem about software development",
+    projects: [
+        {
+            id: 1,
+            name: "human resources, finance and administration System Management",
+            description: "The porpuse of this system management was to increase the number of attendance through a web platform which the final user can interact with it and improve their performance task respectively",
+            frameworks: [".Net Core", "Boostrap"],
+            languages: ["HTML5","CSS3", "JavaScript","C#"],
+            database: "Microsoft SQL Server",
+            sourceCode: "private"
+        },
+        {
+            id: 2,
+            name: "Haat CAIS",
+            description: "The porpuse of this mobile app was to increase product sells about this cultural institution who was charged to sell and manage all this product, art etc.",
+            frameworks: ["React Native"],
+            languages: ["JavaScript"],
+            database: "Firebase",
+            sourceCode: "private"
+        },
+        {
+            id: 3,
+            name: "MIGO",
+            description: "The porpuse of this web app is to storage your movies, series, comics and books preferences through a friendly, beautiful and easy to use layout. You as a user will be able to manage your content preference as you wish, simple and easy",
+            frameworks: ["React.js", ".Net Core"],
+            languages: ["HTML5","CSS3","JavaScript ES6+","C#"],
+            database: "Microsoft SQL Server",
+            sourceCode: "public soon"
+        },
+    ],
+    education: [
+        {
+            id: 1,
+            name: "Hermosillo institute of tecnology",
+            degree: "Informatic Engineering",
+            description: "Institute located in Sonora, Hermosillo city with engineering, finance and administration national prestige"
+        }
+    ],
+    setSkills: {
+        webFundamentals: ["HTML5", "CSS3", "JavaScript ES6+", "Responsive Design"],
+        frameworks: [".Net Core", "React.js", "React Native"],
+        versionControl: ["Git"],
+        database: ["Microsoft SQL Server", "Firebase"],
+        programmingLanguages: ["Java", "C#", "Python", "JavaScript"],
+        methodologies: ["SCRUM"],
+        uiDesign: ["Adobe XD"]
+    },
+    softSkills: ["Skill","Skill","Skill","Skill","Skill","Skill","Skill","Skill",],
+    tongues: ["Spanish", "English - Intermediate B1"]
+}
+
+export default aboutMeData
