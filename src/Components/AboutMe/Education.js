7a14c1674ed367e50f2fae937aa8ef1ac755ecb1
Texmo100 <isaac.ir@hotmail.com>
2021-05-22 21:12:20 -0700

    Education section in about me page fixed correctly

--- src/Components/AboutMe/Education.js
+++ src/Components/AboutMe/Education.js
@@ -4,7 +4,9 @@ const Education = (props) => {
     return (
         <div className="content-item">
             <p className="content-item-title">Education</p>
-            <p className="content-item-text">{`${props.education[0].degree} in ${props.education[0].name} (2016 - 2021)`}</p>
+            <p className="content-item-text" style={{fontWeight: "bold"}}>
+                {`${props.education[0].degree} in ${props.education[0].name} (2016 - 2021).`}
+            </p>
             <p className="content-item-text">{props.education[0].description}</p>
         </div>
     )
