c8c92190a58c8ef7a0ffba7958c7a777bc0defae
Texmo100 <isaac.ir@hotmail.com>
2021-05-25 12:48:33 -0700

    Syntax Error in navbar and MainImage fixed Correctly

--- src/Components/Home/MainImageSection.js
+++ src/Components/Home/MainImageSection.js
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 class MainImageSection extends Component{
     render(){
         return(
-            <section className="main-image">
+            <header className="main-image">
                 <div className="image-layer">
                     <div className="container container-main-image">
                         <h1 className="f-400">Hey there, i'm Isaac I.R. a Frontend Developer</h1>
                         <Link className="button button-main-image" to="/contactme">Let's talk</Link>
                     </div>
                 </div>
-            </section>
+            </header>
         )
     }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <header>
-            <nav className="navbar">
-                <ul className="navbar-list-container">
-                    <li>
-                        <Link className="nav-item" to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to="/aboutme">About me</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to="/projects">Projects</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-item" to="/contactme">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <nav className="navbar">
+            <ul className="navbar-list-container">
+                <li>
+                    <Link className="nav-item" to="/">Home</Link>
+                </li>
+                <li>
+                    <Link className="nav-item" to="/aboutme">About me</Link>
+                </li>
+                <li>
+                    <Link className="nav-item" to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link className="nav-item" to="/contactme">Contact</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
