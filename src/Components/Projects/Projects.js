78a1db22425c9cbb445b706ab342707bb874957d
Texmo100 <isaac.ir@hotmail.com>
2021-05-25 12:16:00 -0700

    Projects page Done alpha version 1.0

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import GlobalHeader from '../GlobalHeader'
+import CardProject from './CardProject'
 import FooterSection from '../FooterSection'
 
 const Projects = () => {
@@ -7,7 +8,14 @@ const Projects = () => {
         <div className="wrapper-inner-pages">
             <GlobalHeader image="header-image-projects"/>
             <div className="content-page">
-                <div className="container-inner-pages"></div>
+                <div className="container-inner-pages">
+                    <h1 className="title-page title-page-projects">Projects</h1>
+                    <div className="container-cards-projects-page">
+                        <CardProject />
+                        <CardProject />
+                        <CardProject />
+                    </div>
+                </div>
             </div>
             <FooterSection />
         </div>
