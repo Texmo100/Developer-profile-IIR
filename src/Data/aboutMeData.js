10dcc9ef001044b90f37a40a0275a8ece86ec7a0
Texmo100 <isaac.ir@hotmail.com>
2021-05-27 15:34:30 -0700

    aboutMeData.js updated

--- src/Data/aboutMeData.js
+++ src/Data/aboutMeData.js
@@ -5,8 +5,34 @@ const aboutMeData = {
             id: 1,
             name: "human resources, finance and administration System Management",
             description: "The porpuse of this system management was to increase the number of attendance through a web platform which the final user can interact with it and improve their performance task respectively.",
-            frameworks: [".Net Core", "Boostrap"],
-            languages: ["HTML5","CSS3", "JavaScript","C#"],
+            frameworks: [
+                {
+                    id: 1,
+                    name: ".Net Core"
+                },
+                {
+                    id: 2,
+                    name: "Boostrap"
+                },
+            ],
+            languages: [
+                {
+                    id: 1,
+                    name: "HTML5"
+                },
+                {
+                    id: 2,
+                    name: "CSS3"
+                },
+                {
+                    id: 3,
+                    name: "JavaScript"
+                },
+                {
+                    id: 4,
+                    name: "C#"
+                },
+            ],
             database: "Microsoft SQL Server",
             sourceCode: "private"
         },
@@ -14,8 +40,18 @@ const aboutMeData = {
             id: 2,
             name: "Haat CAIS",
             description: "The porpuse of this mobile app was to increase product sells about this cultural institution who was charged to sell and manage all this product, art etc.",
-            frameworks: ["React Native"],
-            languages: ["JavaScript"],
+            frameworks: [
+                {
+                    id: 1,
+                    name: "React Native"
+                },
+            ],
+            languages: [
+                {
+                    id: 1,
+                    name: "JavaScript"
+                },
+            ],
             database: "Firebase",
             sourceCode: "private"
         },
@@ -23,8 +59,34 @@ const aboutMeData = {
             id: 3,
             name: "MIGO",
             description: "The porpuse of this web app is to storage your movies, series, comics and books preferences through a friendly, beautiful and easy to use layout. You as a user will be able to manage your content preference as you wish, simple and easy.",
-            frameworks: ["React.js", ".Net Core"],
-            languages: ["HTML5","CSS3","JavaScript ES6+","C#"],
+            frameworks: [
+                {
+                    id: 1,
+                    name: "React.js"
+                },
+                {
+                    id: 2,
+                    name: ".Net Core"
+                },
+            ],
+            languages: [
+                {
+                    id: 1,
+                    name: "HTML5"
+                },
+                {
+                    id: 2,
+                    name: "CSS3"
+                },
+                {
+                    id: 3,
+                    name: "JavaScript ES6+"
+                },
+                {
+                    id: 4,
+                    name: "C#"
+                },
+            ],
             database: "Microsoft SQL Server",
             sourceCode: "public soon"
         },
@@ -38,13 +100,84 @@ const aboutMeData = {
         }
     ],
     setSkills: {
-        webFundamentals: ["HTML5", "CSS3", "JavaScript ES6+", "Responsive Design"],
-        frameworks: [".Net Core", "React.js", "React Native"],
-        versionControl: ["Git"],
-        database: ["Microsoft SQL Server", "Firebase"],
-        programmingLanguages: ["Java", "C#", "Python", "JavaScript"],
-        methodologies: ["SCRUM"],
-        uiDesign: ["Adobe XD"]
+        webFundamentals: [
+            {
+                id: 1,
+                name: "HTML5"
+            },
+            {
+                id: 2,
+                name: "CSS3"
+            },
+            {
+                id: 3,
+                name: "JavaScript ES6+"
+            },
+            {
+                id: 4,
+                name: "Responsive Design"
+            },
+        ],
+        frameworks: [
+            {
+                id: 1,
+                name: ".Net Core"
+            },
+            {
+                id: 2,
+                name: "React.js"
+            },
+            {
+                id: 3,
+                name: "React Native"
+            },
+        ],
+        versionControl: [
+            {
+                id: 1,
+                name: "Git"
+            },
+        ],
+        database: [
+            {
+                id: 1,
+                name: "Microsoft SQL Server"
+            },
+            {
+                id: 2,
+                name: "Firebase"
+            },
+        ],
+        programmingLanguages: [
+            {
+                id: 1,
+                name: "Java"
+            },
+            {
+                id: 2,
+                name: "C#"
+            },
+            {
+                id: 3,
+                name: "Python"
+            },
+            {
+                id: 3,
+                name: "JavaScript"
+            },
+        ],
+        methodologies: [
+            {
+                id: 1,
+                name: "SCRUM"
+            },
+        ],
+        uiDesign: [
+            {
+                id: 1,
+                name: "Adobe XD"
+            },
+        ],
     },
     softSkills: [
         {
