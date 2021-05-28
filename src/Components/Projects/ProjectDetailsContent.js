6bce94fec4fab9524432822c8e2799bd5f9e6035
Texmo100 <isaac.ir@hotmail.com>
2021-05-27 22:21:40 -0700

    Developer Profile Beta version 1.2

--- src/Components/Projects/ProjectDetailsContent.js
+++ src/Components/Projects/ProjectDetailsContent.js
@@ -2,18 +2,13 @@ import React from 'react'
 import { AiFillHtml5 } from 'react-icons/ai';
 import { DiCss3 } from 'react-icons/di';
 import { SiJavascript } from 'react-icons/si';
-import { MdPhonelink } from 'react-icons/md';
 import { SiDotNet } from 'react-icons/si';
 import { FaReact } from 'react-icons/fa';
 import { SiJava } from 'react-icons/si';
 import { SiCsharp } from 'react-icons/si';
 import { SiPython } from 'react-icons/si';
-import { FaGitAlt } from 'react-icons/fa';
 import { SiMicrosoftsqlserver } from 'react-icons/si';
 import { SiFirebase } from 'react-icons/si';
-import { DiScrum } from 'react-icons/di';
-import { FaTrello } from 'react-icons/fa';
-import { SiAdobexd } from 'react-icons/si';
 import { SiBootstrap } from 'react-icons/si';
 
 const ProjectDetailsContent = (props) => {
@@ -54,6 +49,11 @@ const ProjectDetailsContent = (props) => {
                 </div>
             )
         }
+        return (
+            <div className="icon" key={item.id}>
+                <p className="icon-name">{item.name}</p>
+            </div>
+        )
     })
 
     //Database Render
@@ -73,6 +73,11 @@ const ProjectDetailsContent = (props) => {
                 </div>
             )
         }
+        return (
+            <div className="icon" key={item.id}>
+                <p className="icon-name">{item.name}</p>
+            </div>
+        )
     })
 
     //Languages Render
@@ -120,6 +125,11 @@ const ProjectDetailsContent = (props) => {
                 </div>
             )
         }
+        return (
+            <div className="icon" key={item.id}>
+                <p className="icon-name">{item.name}</p>
+            </div>
+        )
     })
 
     //Source Code Render
